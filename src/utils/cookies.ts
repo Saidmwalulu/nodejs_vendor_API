4ea2731dc3f66ec0fbc067648a5e42a0dd2130e9
Said <saidmwalulu@gmail.com>
2025-11-03 17:04:12 +0300

    cookie-issue

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,8 +1,9 @@
+import { path } from 'path';
 import { CookieOptions, Response } from "express";
 import { fifteenMinutesFromNow, thirtyDaysFromNow } from "./date";
 import { NODE_ENV } from "./env";
 
-const secure = process.env.NODE_ENV !== "development";
+//const secure = process.env.NODE_ENV !== "development";
 const isProduction = NODE_ENV === "production";
 
 export const REFRESH_PATH = "/auth/refresh";
@@ -16,6 +17,7 @@ const defaults: CookieOptions = {
 export const getAccessTokenCookieOptions = () => ({
   ...defaults,
   expires: fifteenMinutesFromNow(),
+  path: "/",
 });
 
 export const getRefreshTokenCookieOptions = () => ({
